9f5b9e371572314d64b9f8f3d1ec86514f91c4af
Jangidp0903 <puneetjangid637@gmail.com>
2025-05-01 17:37:44 +0530

    add env for token

--- controller/user.Controller.js
+++ controller/user.Controller.js
@@ -59,8 +59,8 @@ export const loginUser = async (req, res, next) => {
     res.cookie("token", token, {
       httpOnly: true,
       expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
-      sameSite: "None",
-      secure: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
     });
 
     res.status(200).json({
