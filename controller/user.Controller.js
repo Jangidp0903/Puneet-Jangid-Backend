8c4d7ceb3d3ea1c8db7ae3bcbc903015eb2b1f6d
Jangidp0903 <156504199+Jangidp0903@users.noreply.github.com>
2026-01-27 20:06:51 +0530

    minor

--- controller/user.Controller.js
+++ controller/user.Controller.js
@@ -40,19 +40,19 @@ export const loginUser = async (req, res, next) => {
     res
       .cookie('accessToken', accessToken, {
         httpOnly: true,
-        secure: true,
-        sameSite: 'None',
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
         maxAge: 15 * 60 * 1000, // 15 minute
       })
       .cookie('refreshToken', refreshToken, {
         httpOnly: true,
-        secure: true,
-        sameSite: 'None',
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       });
 
     res.status(200).json({
-      message: 'User logged in successfully',
+      message: 'User logged in successfully and cookies set and EC2 instance created',
       accessToken,
       success: true,
       user: userData,
